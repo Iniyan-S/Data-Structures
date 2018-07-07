fc0fe81f703e83f7564f21b54f81bcc228e308c0
Iniyan <10035395+Iniyan-S@users.noreply.github.com>
2018-07-07 16:43:06 +0530

    Removed create_dataset() function

--- DictionaryObject.py
+++ DictionaryObject.py
@@ -1,11 +1,1 @@
 class_names = ["jack", "bob", "mary", "jeff", "ann", "pierre", "martha", "clause", "pablo", "susan", "gustav"]
-
-def create_dataset():
-    import randon
-    num_entries = 5000000
-    f = open("data.txt", "w")
-    for i in range(num_entries):
-        current = random.choice(class_names)
-        f.write(current+"\n")
-
-    f.close()
