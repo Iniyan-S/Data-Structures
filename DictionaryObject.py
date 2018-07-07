578002c1da03a9ec477376e6b925e74cecfec34b
Iniyan-S <ediscipulus@gmail.com>
2018-07-07 23:31:17 +0530

    Added create_dataset() in create_dataset branch

--- DictionaryObject.py
+++ DictionaryObject.py
@@ -1,3 +1,11 @@
 class_names = ["jack", "bob", "mary", "jeff", "ann", "pierre", "martha", "clause", "pablo", "susan", "gustav"]
 
 # Create a dataset function
+def create_dataset():
+    import random
+    num_entries = 5000000
+    f = open("data.txt", "w")
+    for i in num_entries:
+        current = random.choice(class_names)
+        f.write(current + \n)
+    f.close()
